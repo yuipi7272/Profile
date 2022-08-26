15e794d422bf1530003e789b91153ce5aed78ce5
YuiOgawa <yui20010218@gmail.com>
2022-08-27 01:09:30 +0900

    :sparkles: 教科書3-4完成
    ボタンを押した時に表示させる関数の追加

--- Profile/ViewController.swift
+++ Profile/ViewController.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var profileImageView: UIImageView!
+    @IBOutlet var profileCommentLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        profileImageView.isHidden = true
+        profileCommentLabel.isHidden = true
+        
     }
 
-
+    // プロフィールボタンを押したときの動作
+    @IBAction func tapButton(){
+        profileImageView.isHidden = false
+        profileCommentLabel.isHidden = false
+    }
 }
 
